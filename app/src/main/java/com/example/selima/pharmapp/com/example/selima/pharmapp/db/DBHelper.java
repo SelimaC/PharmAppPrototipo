be0551b58a527c90c692052bf0894f500c9750e6
SelimaCurci <seliii@hotmail.it>
2017-02-16 10:10:28 +0100

    class dbhelper

---
+++ app/src/main/java/com/example/selima/pharmapp/com/example/selima/pharmapp/db/DBHelper.java
@@ -0,0 +1,32 @@
+package com.example.selima.pharmapp.com.example.selima.pharmapp.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by selim on 16/02/2017.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "MyDBName.db";
+
+    public DBHelper(Context context){
+        super(context,DATABASE_NAME,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //Creation of DB
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
+    /* Put here helper methods like inert data and execute query*/
+}
